03d236c05c638c4d61caac34cdb732bae700166a
Hussein Omar <husseinomar6190@gmail.com>
2022-07-10 21:16:32 +0300

    product posting and vendors dashboard

--- frontend/src/pages/DashCustomers.jsx
+++ frontend/src/pages/DashCustomers.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DashCustomers = () => {
   return (
     <div className='pt-3 pb-2'>
-      <h5>Customers to have interacted</h5>
+      <h5>Customers to have interacted and purchased from this user</h5>
     </div>
   );
 };
