a31583d79e53d1cdd6c98f7ce1077c455f46cb71
Sammy Muchiri <dev.miano@gmail.com>
2022-07-12 00:52:22 +0300

    removed function to get categories

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,12 +1,8 @@
-import axios from 'axios';
 import create from 'zustand';
 import { persist } from 'zustand/middleware';
 
 let store = (set) => ({
 	count: 1,
-	categories: [],
-	loading: false,
-	hasErrors: false,
 
 	increment: () =>
 		set((state) => ({
@@ -19,75 +15,29 @@ let store = (set) => ({
 		})),
 
 	reset: () => set({ count: 1 }),
-
-	addCategories: (category) => {
-		set((state) => ({
-			categories: [...state.categories, category],
-		}));
-	},
-
-	fetchCategories: async () => {
-		set(() => ({ loading: true }));
-		try {
-			const response = await axios.get(
-				'https://fichuastore.herokuapp.com/api/shop/categories/'
-			);
-			set((state) => ({
-				categories: (state.categories = response.data),
-				loading: false,
-			}));
-		} catch (err) {
-			set(() => ({ hasErrors: true, loading: false }));
-		}
-	},
 });
 
 store = persist(store, { name: 'store' });
 
 export const useStore = create(store);
 
-// export const useStore = create((set) => ({
-// 	count: 1,
-// 	categories: [],
-// 	loading: false,
-// 	hasErrors: false,
-// 	increment: () =>
+// fetchCategories: async () => {
+// 	set(() => ({ loading: true }));
+// 	try {
+// 		const response = await axios.get(
+// 			'https://fichuastore.herokuapp.com/api/shop/categories/'
+// 		);
 // 		set((state) => ({
-// 			count: state.count + 1,
-// 		})),
+// 			categories: (state.categories = response.data),
+// 			loading: false,
+// 		}));
+// 	} catch (err) {
+// 		set(() => ({ hasErrors: true, loading: false }));
+// 	}
+// },
 
-// 	decrement: () =>
-// 		set((state) => ({
-// 			count: state.count - 1,
-// 		})),
-
-// 	reset: () => set({ count: 1 }),
-
-// 	fetchCategories: async () => {
-// 		set(() => ({ loading: true }));
-
-// 		try {
-// 			const response = await axios.get(
-// 				'https://fichuastore.herokuapp.com/api/shop/categories/'
-// 			);
-
-// 			set((state) => ({
-// 				categories: (state.categories = response.categories),
-// 				loading: false,
-// 			}));
-// 		} catch (err) {
-// 			set(() => ({ hasErrors: true, loading: false }));
-// 		}
-// 	},
-// }));
-
-// let getCategories = (set) => ({
-// 	categoryAPI: categoryAPI,
-// 	categories: [],
-
-// 	fetchCategories: async (categoryAPI) => {
-// 		const response = await fetch(categoryAPI);
-// 		const json = await response.json();
-// 		set({ categories: json.items });
-// 	},
-// });
+// 	addCategories: (category) => {
+// 	set((state) => ({
+// 		categories: [...state.categories, category],
+// 	}));
+// },
--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,22 +1,6 @@
 import React from 'react';
-import { useStore } from '../app/store';
-import SingleCategory from './SingleCategory';
-import Spinner from './Spinner';
 
 function Categories() {
-	const categories = useStore((state) => state.categories);
-	const loading = useStore((state) => state.loading);
-	const hasErrors = useStore((state) => state.hasErrors);
-	// const fetchCategories = useStore((state) => state.fetchCategories);
-
-	if (loading) {
-		return <Spinner />;
-	}
-
-	if (hasErrors) {
-		return <p>cannot read Categories</p>;
-	}
-
 	return (
 		<div className='container mt-5'>
 			<div className='title text-center'>
@@ -26,17 +10,6 @@ function Categories() {
 				<span className='textC text-center text-dark'>
 					Finding Best Products Now in Your Fingertips
 				</span>
-			</div>
-			{/* <button onClick={fetchCategories}>get category</button> */}
-			<div className='row mt-2 g-4'>
-				{categories.map((category) => (
-					<SingleCategory
-						key={category.id}
-						slug={category.slug}
-						name={category.name}
-						categoryImage={category.image}
-					/>
-				))}
 			</div>
 		</div>
 	);
