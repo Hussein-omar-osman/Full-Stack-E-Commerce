4cabccaedc56641da25e9ddfb27f6b03c5376072
Sammy Muchiri <dev.miano@gmail.com>
2022-07-12 23:41:31 +0300

    truncated name variable

--- frontend/src/components/SingleProduct.jsx
+++ frontend/src/components/SingleProduct.jsx
@@ -4,47 +4,49 @@ import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { LinkContainer } from 'react-router-bootstrap';
 
 function SingleProduct({ id, slug, name, price, photo }) {
-  return (
-    <div className='m-1 p-2 bg-light rounded-3' id={id}>
-      <LinkContainer to={`/product/${id}`}>
-        <div className=''>
-          <div className='special-img collection-img position-relative'>
-            <img
-              src={`https://res.cloudinary.com/fichua-store/${photo}`}
-              className='w-100 rounded-3'
-              alt=''
-              style={{ width: '360px', height: '240px', objectFit: 'cover' }}
-            />
-          </div>
-          <div className='text-start'>
-            <p className='fw-bold text-capitalize my-1'>{name}</p>
-            <div className='rating'>
-              <span className='text-secondary'>
-                <FontAwesomeIcon icon={solid('star')} />
-              </span>
-              <span className='text-secondary'>
-                <FontAwesomeIcon icon={solid('star')} />
-              </span>
-              <span className='text-secondary'>
-                <FontAwesomeIcon icon={solid('star')} />
-              </span>
-              <span className=''>
-                <FontAwesomeIcon icon={solid('star')} />
-              </span>
-              <span className=''>
-                <FontAwesomeIcon icon={solid('star')} />
-              </span>
-            </div>
-            <div>
-              <span className='fw-bold text-lg-start'>
-                <span className='fw-light'>KES</span> {price}
-              </span>
-            </div>
-          </div>
-        </div>
-      </LinkContainer>
-    </div>
-  );
+	return (
+		<div className='m-1 p-2 bg-light rounded-3' id={id}>
+			<LinkContainer to={`/product/${id}`}>
+				<div className=''>
+					<div className='special-img collection-img position-relative'>
+						<img
+							src={`https://res.cloudinary.com/fichua-store/${photo}`}
+							className='w-100 rounded-3'
+							alt=''
+							style={{ width: '360px', height: '240px', objectFit: 'cover' }}
+						/>
+					</div>
+					<div className='text-start'>
+						<p className='fw-bold text-capitalize my-1'>
+							{name.substring(0, 55)} {name.length >= 48 && '...'}
+						</p>
+						<div className='rating'>
+							<span className='text-secondary'>
+								<FontAwesomeIcon icon={solid('star')} />
+							</span>
+							<span className='text-secondary'>
+								<FontAwesomeIcon icon={solid('star')} />
+							</span>
+							<span className='text-secondary'>
+								<FontAwesomeIcon icon={solid('star')} />
+							</span>
+							<span className=''>
+								<FontAwesomeIcon icon={solid('star')} />
+							</span>
+							<span className=''>
+								<FontAwesomeIcon icon={solid('star')} />
+							</span>
+						</div>
+						<div>
+							<span className='fw-bold text-lg-start'>
+								<span className='fw-light'>KES</span> {price}
+							</span>
+						</div>
+					</div>
+				</div>
+			</LinkContainer>
+		</div>
+	);
 }
 
 export default SingleProduct;
