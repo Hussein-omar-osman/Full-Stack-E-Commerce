8422836ded8ca70baf5d3cc196cc92f571a23880
Sammy Muchiri <dev.miano@gmail.com>
2022-07-12 03:51:20 +0300

    added function to fetch all products

--- frontend/src/pages/shop/Shop.jsx
+++ frontend/src/pages/shop/Shop.jsx
@@ -1,9 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Footer, SingleProduct } from '../../components';
-import Coat from '../../assets/images/c_t-shirt_men.png';
 import Container from 'react-bootstrap/Container';
 
 const Shop = () => {
+	const [products, setProducts] = useState([]);
+
+	const fetchProducts = async () => {
+		let res = await fetch(
+			'https://fichuastore.herokuapp.com/api/shop/products/'
+		);
+		let data = await res.json();
+		console.log(data);
+		setProducts(data);
+	};
+
+	useState(() => {
+		fetchProducts();
+	}, []);
+
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
@@ -18,7 +32,7 @@ const Shop = () => {
 						<div className='d-flex flex-wrap justify-content-center mt-5 filter-button-group'>
 							<button
 								type='button'
-								className='btn m-2 text-dark active-filter-btn'>
+								className='btn m-2 text-white active-filter-btn'>
 								All
 							</button>
 							<button type='button' className='btn m-2 text-dark'>
@@ -35,14 +49,15 @@ const Shop = () => {
 							</button>
 						</div>
 
-						<div className='special-list mt-4 row gx-0 gy-3'>
-							<SingleProduct name={'Coat'} price={'$ 49.00'} prodImage={Coat} />
-							<SingleProduct name={'Coat'} price={'$ 49.00'} prodImage={Coat} />
-							<SingleProduct name={'Coat'} price={'$ 49.00'} prodImage={Coat} />
-							<SingleProduct name={'Coat'} price={'$ 49.00'} prodImage={Coat} />
-							<SingleProduct name={'Coat'} price={'$ 49.00'} prodImage={Coat} />
-							<SingleProduct name={'Coat'} price={'$ 49.00'} prodImage={Coat} />
-							<SingleProduct name={'Coat'} price={'$ 49.00'} prodImage={Coat} />
+						<div className='gw'>
+							{products.map((item) => (
+								<SingleProduct
+									name={item.name}
+									photo={item.photo}
+									price={item.price}
+									slug={item.slug}
+								/>
+							))}
 						</div>
 					</div>
 				</div>
