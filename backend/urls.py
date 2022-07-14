f74caf263ea9733a4a02a4e89dbad64e27a90ae5
Sammy Muchiri <dev.miano@gmail.com>
2022-07-14 13:02:27 +0300

    changed url heirachy

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from backend.views import *
 
 urlpatterns = [
-    path('user/<pk>/', get_single_user),
     path('user/become-vendor/', become_vendor),
+    path('user/<pk>/', get_single_user),
     path('profile/<pk>/', ProfileView.as_view()),
 ]
