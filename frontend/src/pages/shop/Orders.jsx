bd94e95d0b53c32e9e98aae801e48d1bddf4552b
Sammy Muchiri <dev.miano@gmail.com>
2022-07-15 11:16:43 +0300

    changed url heirachy

--- frontend/src/pages/shop/Orders.jsx
+++ frontend/src/pages/shop/Orders.jsx
@@ -13,53 +13,51 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import CartContext from '../../context/CartContext';
 
 const Orders = () => {
-  const { cart, setCart } = useContext(CartContext);
-  let total = 0;
-  for (let c of cart) {
-    total = total + c.amount;
-  }
+	const { cart, setCart } = useContext(CartContext);
+	let total = 0;
+	for (let c of cart) {
+		total = total + c.amount;
+	}
 
-  return (
-    <Container className='p-5'>
-      <h1 className='px-4 text-primary'>Your Cart</h1>
-      <Container className='mt-4'>
-        {cart.map((item) => (
-          <OrderCard key={item.id} product={item} />
-        ))}
-        <Card border='secondary' className='mt-3 pe-5 pt-2'>
-          <CardGroup className='justify-content-end align-items-center p-2'>
-            <Card.Title className='text-primary pe-3'>Total amount:</Card.Title>
-            <Card.Title className='text-dark'>KES {total}</Card.Title>
-          </CardGroup>
-        </Card>
-      </Container>
+	return (
+		<Container className='p-5'>
+			<h1 className='px-4 text-primary'>Your Cart</h1>
+			<Container className='mt-4'>
+				{cart.map((item) => (
+					<OrderCard key={item.id} product={item} />
+				))}
+				<Card border='secondary' className='mt-3 pe-5 pt-2'>
+					<CardGroup className='justify-content-end align-items-center p-2'>
+						<Card.Title className='text-primary pe-3'>Total amount:</Card.Title>
+						<Card.Title className='text-dark'>KES {total}</Card.Title>
+					</CardGroup>
+				</Card>
+			</Container>
 
-      <Stack
-        className='justify-content-between mt-3 px-2'
-        direction='horizontal'
-      >
-        <Nav.Item className=''>
-          <LinkContainer className='' to='/shop'>
-            <Button
-              variant='outline-primary'
-              className='txt-b btn-w'
-              onClick={<Navigate to='/shop' replace={true} />}
-            >
-              Back
-            </Button>
-          </LinkContainer>
-        </Nav.Item>
-        <Nav.Item className=''>
-          <LinkContainer className='' to='/cart/shipping'>
-            <Button className='txt-b'>
-              Continue
-              <MdOutlineArrowForward className=' ms-2' />
-            </Button>
-          </LinkContainer>
-        </Nav.Item>
-      </Stack>
-    </Container>
-  );
+			<Stack
+				className='justify-content-between mt-3 px-2'
+				direction='horizontal'>
+				<Nav.Item className=''>
+					<LinkContainer className='' to='/shop'>
+						<Button
+							variant='outline-primary'
+							className='txt-b btn-w'
+							onClick={<Navigate to='/shop' replace={true} />}>
+							Back
+						</Button>
+					</LinkContainer>
+				</Nav.Item>
+				<Nav.Item className=''>
+					<LinkContainer className='' to='/cart/shipping'>
+						<Button className='txt-b text-white'>
+							Continue
+							<MdOutlineArrowForward className=' ms-2' />
+						</Button>
+					</LinkContainer>
+				</Nav.Item>
+			</Stack>
+		</Container>
+	);
 };
 
 export default Orders;
